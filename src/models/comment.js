8cd320e009310f516328e5cb3162a2bf1b987c46
IS <1588186+issarapong@users.noreply.github.com>
2023-05-25 15:27:15 +0700

    Finish:SyncDB

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,28 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define(
-    "Comment",
-    {
-      message: DataTypes.STRING,
-    },
-    {
-      underscored: true,
-    }
-  );
-  Comment.associate = (models) => {
-    Comment.belongsTo(models.User, {
-      foreignKey: {
-        name: "userId",
-        allowNull: false,
+    const Comment = sequelize.define(
+      'Comment',
+      {
+        message: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            notEmpty: true
+          }
+        }
       },
-      onDelete: "RESTRICT",
-    });
-    Comment.belongsTo(models.Post, {
-      foreignKey: {
-        name: "postId",
-        allowNull: false,
-      },
-      onDelete: "RESTRICT",
-    });
+      {
+        underscored: true
+      }
+    );
+  
+    Comment.associate = models => {
+      Comment.belongsTo(models.User, {
+        foreignKey: {
+          name: 'userId',
+          allowNull: false
+        },
+        onDelete: 'RESTRICT'
+      });
+  
+      Comment.belongsTo(models.Post, {
+        foreignKey: {
+          name: 'postId',
+          allowNull: false
+        },
+        onDelete: 'RESTRICT'
+      });
+    };
+  
+    return Comment;
   };
-  return Comment;
-};
